6bdd6013a019efb090c192fb57e7dcc42181d702
Banaanae <will.litch@outlook.com>
2024-01-18 15:55:18 +1100

    make website amazing

---
+++ index.js
@@ -0,0 +1,30 @@
+let random = document.getElementById('random')
+let marrace = document.getElementById('marquee')
+let start = document.getElementById('start')
+
+function updateDirection() {
+    let int = Math.floor(Math.random() * 4);
+    let dir;
+    switch (int) {
+        case 0: dir = 'left'; break;
+        case 1: dir = 'right'; break;
+        case 2: dir = 'up'; break;
+        case 3: dir = 'down'; break;
+    }
+    random.setAttribute('direction', dir);
+}
+
+setInterval(updateDirection, 2500);
+
+document.getElementById('start').addEventListener('click', createBtn)
+function createBtn() {
+    var nextButton = document.createElement('button');
+    nextButton.innerText = 'One Click = One Marquee Saved';
+    var newMarquee = document.createElement('marquee');
+    newMarquee.behavior = 'scroll';
+    newMarquee.direction = 'left';
+    nextButton.addEventListener('click', createBtn)
+    document.getElementById('container').appendChild(newMarquee);
+    newMarquee.appendChild(nextButton);
+    document.getElementById('container').appendChild(document.createElement('br'));
+};
